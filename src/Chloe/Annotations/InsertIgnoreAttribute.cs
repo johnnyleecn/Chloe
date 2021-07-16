dc242ebf09417c18ab034ece133e7cc3fbf74e28
iamliqiang126_code <50851354@qq.com>
2021-07-16 15:11:33 +0800

    20210629-1848：增加备注，说明InsertIgnoreAttribute只适用于SqlServer

--- src/Chloe/Annotations/InsertIgnoreAttribute.cs
+++ src/Chloe/Annotations/InsertIgnoreAttribute.cs
@@ -3,7 +3,7 @@
 namespace Chloe.Annotations
 {
     /// <summary>
-    /// 插入时忽略（李强新增专用）
+    /// 插入时忽略（李强新增专用，只在MsSql中适用，未改其他数据库引擎）
     /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
     public class InsertIgnoreAttribute : Attribute
